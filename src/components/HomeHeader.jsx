b9b9223e25bbd926db3ad572c5003294463a2069
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-05-13 19:02:57 +0200

    home update

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,9 +1,22 @@
-import homeImage from '../assets/Home-Hero-Image.jpg'
+import homeImage from '../assets/Home-Hero-Image.jpg';
+import decorationline from '../assets/Decoration.svg';
 
 const HomeHeader = () => {
-    return (<>
-    <img src={homeImage} className='home__image'></img>
-    </>)
+    return (
+    <section className='home__homeheader container'>
+        <div>
+            <img src={homeImage} className='home__homeheader__image'></img>
+        </div>
+        <div className='home__homeheader__text'>
+            <h2>Zacznij pomagać!</h2>
+            <h2>Oddaj niechciane rzeczy w zaufane ręce</h2>
+            <img src={decorationline} alt="line"/>
+            <div>
+                <button>ODDAJ RZECZY</button>
+                <button>ZORGANIZUJ ZBIÓRKĘ</button>
+            </div>
+        </div>
+    </section>)
 }
 
 export default HomeHeader
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,9 @@
 import HomeHeader from './HomeHeader';
+import * as Scroll from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 const Home = () => {
     return (<section className='home'>
-    Home
     <HomeHeader className='container'/>
     </section>)
 }
