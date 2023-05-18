eacf7cbb9450410e38560f1c37c89804e25172f8
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-05-18 17:17:36 +0200

    login update

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -6,7 +6,6 @@ export const LoginForm = () => {
     <h2>Strona w budowie</h2>
     <div className='home__homeheader__buttons'>
         <Link to="/" className='button'>Wracam na główną stronę</Link>
-        <Link to="/rejestracja" className='button'>Rejestracja</Link>
     </div>
     </section>)
 }
