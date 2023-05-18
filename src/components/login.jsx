72ee3fa31ff5ab1ecdf3c8d8f3c22be14113a803
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-05-18 15:55:16 +0200

    login and register form update

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,12 @@
-
+import { Link } from 'react-router-dom';
 
 export const LoginForm = () => {
-    return (<>
+    return (<section className='LoginForm'>
     <h1>LOGOWANIE</h1>
-    Strona w budowie
-    </>)
+    <h2>Strona w budowie</h2>
+    <div className='home__homeheader__buttons'>
+        <Link to="/" className='button'>Wracam na główną stronę</Link>
+        <Link to="/rejestracja" className='button'>Rejestracja</Link>
+    </div>
+    </section>)
 }
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export const RegisterForm = () => {
-    return (<>
+    return (<section className='LoginForm'>
     <h1>REJESTRACJA</h1>
-    Strona w budowie
-    </>)
+    <h2>Strona w budowie</h2>
+    <div>
+        <Link to="/" className='button'>Wracam na główną stronę</Link>
+    </div>
+    </section>)
 }
