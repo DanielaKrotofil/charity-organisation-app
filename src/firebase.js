71e89bee34816e0707bbe306169d0e64b0cb4405
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-05-20 21:55:49 +0200

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCu_vcz59l5aaVJJvFU1wCA0gyvK8Ylqhk",
+  authDomain: "oddaj-rzeczy-projekt.firebaseapp.com",
+  projectId: "oddaj-rzeczy-projekt",
+  storageBucket: "oddaj-rzeczy-projekt.appspot.com",
+  messagingSenderId: "809936677730",
+  appId: "1:809936677730:web:5437815d23e8f9b2951a3b",
+  measurementId: "G-G0BMGMKGX6"
+};
+
+const app = initializeApp(firebaseConfig);
