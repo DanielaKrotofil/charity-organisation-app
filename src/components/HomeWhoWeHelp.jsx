665665db6da0d8bac83c93d51775bff073439e40
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-05-20 22:08:20 +0200

    who we help update

--- src/components/HomeWhoWeHelp.jsx
+++ src/components/HomeWhoWeHelp.jsx
@@ -1,10 +1,12 @@
 import HomeFundations from "./HomeFundations";
 import HomeOrganizations from "./HomeOrganizations";
 import HomeLocalFundraisers from "./HomeLocalFundraisers";
+import decorationline from '../assets/Decoration.svg';
 
 const WhoWeHelp = () =>{
     return (<>
     <h2>Komu pomagamy?</h2>
+    <img className='decoration-line' src={decorationline} alt="line"/>
     <HomeFundations/>
     <HomeOrganizations/>
     <HomeLocalFundraisers/>
