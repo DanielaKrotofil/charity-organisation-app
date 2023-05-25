a5d54c5b19c12c46b7323e13c56a66c0ef3330ec
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-05-25 18:50:01 +0200

    who we help update

--- src/components/HomeWhoWeHelp.jsx
+++ src/components/HomeWhoWeHelp.jsx
@@ -1,85 +1,85 @@
 import decorationline from '../assets/Decoration.svg';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { collection, getDocs } from "firebase/firestore";
 import { db } from '../firebase';
 
-const WhoWeHelp = () =>{
-    const [dataToRender, setDataToRender] = useState(null);
-    const [fundations, setFundations] = useState([]);
+const WhoWeHelp = () => {
+  const [fundations, setFundations] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "fundations"));
+      const tempArr = [];
+      querySnapshot.forEach((doc) => {
+        tempArr.push(doc.data());
+        console.log(doc.id, " => ", doc.data());
+      });
+      setFundations(tempArr);
+      setInstitution(tempArr);
+    };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        getDocs(collection(db, "fundations")).then((querySnapshot) => {
-            const tempArr = [];
-            querySnapshot.forEach((doc) => {
-                tempArr.push(doc.data());
-                console.log(doc.id, " => ", doc.data());
-            })
+  const [organizations, setOrganizations] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "organizations"));
+      const tempArr = [];
+      querySnapshot.forEach((doc) => {
+        tempArr.push(doc.data());
+        console.log(doc.id, " => ", doc.data());
+      });
+      setOrganizations(tempArr);
+    };
+    fetchData();
+  }, []);
 
-            setFundations(tempArr);
-        });
-    }, []);
+  const [locals, setLocals] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "LocalFundraisers"));
+      const tempArr = [];
+      querySnapshot.forEach((doc) => {
+        tempArr.push(doc.data());
+        console.log(doc.id, " => ", doc.data());
+      });
+      setLocals(tempArr);
+    };
+    fetchData();
+  }, []);
 
-    const [organizations, setOrganizations] = useState([]);
-    useEffect(() => {
-        getDocs(collection(db, "organizations")).then((querySnapshot) => {
-            const tempArr = [];
-            querySnapshot.forEach((doc) => {
-                tempArr.push(doc.data());
-                console.log(doc.id, " => ", doc.data());
-            })
+  const [institution, setInstitution] = useState([]);
 
-            setOrganizations(tempArr);
-        });
-    }, [])
+  const handleClick = (id) => {
+    console.log(`ID: ${id}`);
+  };
 
-    const [locals, setLocals] = useState([]);
-    useEffect(() => {
-        getDocs(collection(db, "LocalFundraisers")).then((querySnapshot) => {
-            const tempArr = [];
-            querySnapshot.forEach((doc) => {
-                tempArr.push(doc.data());
-                console.log(doc.id, " => ", doc.data());
-            })
-
-            setLocals(tempArr);
-        });
-    }, []);
-
-	const [backgroundColor, setBackgroundColor] = useState("blue");
-
-
-	const change = (backgroundColor) => {
-		setBackgroundColor(backgroundColor);
-	};
-
-    const handleClick = (id) => {
-        console.log(`ID: ${id}`);
-      };
-
-	return (
-		<>
-        <h2>Komu pomagamy?</h2>
-        <img className='decoration-line' src={decorationline} alt="line"/>
-        <div>
-            <button className="button__whowehelp" onClick={() => change("orange")}>Fundajcom</button>
-			<button className="button__whowehelp" onClick={() => change("blue")}>Organizacjom pozarządowym</button>
-			<button className="button__whowehelp" onClick={() => change("green")}>Lokalnym zbiórkom</button>
-        </div>
-        <div className="whowehelp__container">
-            <div
-                style={{
-                        height: '100px',
-                        width: '100px',
-                        backgroundColor: backgroundColor,
-                }}>
-                <button onClick={() => handleClick(1)}>1</button>
-                <button onClick={() => handleClick(2)}>2</button>
-                <button onClick={() => handleClick(3)}>3</button>
-            </div>
-        </div>
-		</>
-	);
-
+  return (
+    <>
+      <h2>Komu pomagamy?</h2>
+      <img className='decoration-line' src={decorationline} alt="line"/>
+      <div>
+        <button className='button__whowehelp' onClick={() => setInstitution(fundations)}>Fundajcom</button>
+        <button className='button__whowehelp' onClick={() => setInstitution(organizations)}>Organizacjom pozarządowym</button>
+        <button className='button__whowehelp' onClick={() => setInstitution(locals)}>Lokalnym zbiórkom</button>
+      </div>
+      <div className="whowehelp__container">
+        {institution.map(({index, buttonName, description}) => (
+          <div className="wedding-styles__element" key={index}>
+            <p>{buttonName}</p>
+            <p>{description}</p>
+          </div>
+        ))}
+        <button onClick={() => handleClick(1)}>1</button>
+        <button onClick={() => handleClick(2)}>2</button>
+        <button onClick={() => handleClick(3)}>3</button>
+      </div>
+    </>
+  );
 }
 
-export default WhoWeHelp
+export default WhoWeHelp;
+/*
+goal
+needs
+title
+*/
