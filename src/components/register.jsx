84c16c3bc211d467b912a5624d081703fb5ce382
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-05-27 12:20:31 +0200

    register form update

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -2,24 +2,57 @@ import { Link } from 'react-router-dom';
 import decorationline from '../assets/Decoration.svg';
 
 export const RegisterForm = () => {
-    return (<section className='LoginForm '>
-    <h2>Zarejestruj się</h2>
-    <img className='decoration-line' src={decorationline} alt="line"/>
-    <span className='LoginForm__main registration'>
-    <form>
-        <label htmlFor="email">Email</label>
-        <input type="email" />
+    return (<>
+    <header className="nav container">
+        <nav className="nav__container__actions nav-loginForm">
+          <ul>
+            <li>
+              <Link to='/' >
+                Start
+              </Link>
+            </li>
+            <li>
+              <Link to='/'>
+                O co chodzi?
+              </Link>
+            </li>
+            <li>
+              <Link to='/'>
+                O nas
+              </Link>
+            </li>
+            <li>
+              <Link to='/'>
+                Fundacje i organizacje
+              </Link>
+            </li>
+            <li>
+              <Link to='/'>
+                Kontakt
+              </Link>
+            </li>
+          </ul>
+        </nav>
+    </header>
+    <section className='LoginForm '>
+        <h2>Zarejestruj się</h2>
+        <img className='decoration-line' src={decorationline} alt="line"/>
+        <span className='LoginForm__main registration'>
+        <form>
+            <label htmlFor="email">Email</label>
+            <input type="email" />
 
-        <label htmlFor="password">Hasło</label>
-        <input type="password"/>
+            <label htmlFor="password">Hasło</label>
+            <input type="password"/>
 
-        <label htmlFor="password">Powtórz hasło</label>
-        <input type="password"/>
-    </form>
-    </span>
-    <div className='LoginForm-buttons'>
-        <Link to="/logowanie" className='button noborder'>Zaloguj się</Link>
-        <Link to="/rejestracja" className='button'>Załóż konto</Link>
-    </div>
-    </section>)
+            <label htmlFor="password">Powtórz hasło</label>
+            <input type="password"/>
+        </form>
+        </span>
+        <div className='LoginForm-buttons'>
+            <Link to="/logowanie" className='button noborder'>Zaloguj się</Link>
+            <Link to="/rejestracja" className='button'>Załóż konto</Link>
+        </div>
+    </section>
+    </>)
 }
